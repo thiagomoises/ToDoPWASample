e778eaf9ff7926672822a86d75ebb5505af3b97b
moises <thiago.moises@outlook.com.br>
2019-01-23 13:20:05 -0200

    Ajustes para transformar em PWA

--- PWATodo/src/app/app.module.ts
+++ PWATodo/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,8 @@ import { TodoComponent } from './todo/todo.component';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
